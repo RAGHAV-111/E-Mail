c2fb261f53d57bee915a9a728ae937b571dcf206
Raghav Dwivedi <94631500+RAGHAV-111@users.noreply.github.com>
2023-07-13 11:48:54 +0530

    Add files via upload

---
+++ src/components/pages/Banner.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+import './Banner.css'
+const Banner = () => {
+   
+    return (
+        <Fragment>
+        <div className='home-container'>
+            <div>Welcome </div>
+        </div>
+        </Fragment>
+    )
+}
+export default Banner;
